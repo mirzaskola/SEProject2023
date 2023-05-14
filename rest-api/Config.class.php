8ed25d44ae64dbb1c04f49d8bd5f5c4085eaf628
mirzaskola <mirza.kurtovic@stu.ibu.edu.ba>
2023-05-14 15:28:04 +0200

    Updated config.php

--- rest-api/Config.class.php
+++ rest-api/Config.class.php
@@ -3,19 +3,19 @@
 class Config {
 
   public static function DB_HOST(){
-    return Config::get_env("DB_HOST", "sql7.freemysqlhosting.net");
+    return Config::get_env("DB_HOST", "db-mysql-fra1-22212-do-user-14100799-0.b.db.ondigitalocean.com");
   }
   public static function DB_USERNAME(){
-    return Config::get_env("DB_USERNAME", "sql7616663");
+    return Config::get_env("DB_USERNAME", "doadmin");
   }
   public static function DB_PASSWORD(){
-    return Config::get_env("DB_PASSWORD", "E3l8kZU7xh");
+    return Config::get_env("DB_PASSWORD", "AVNS_fzFjnSXu9-Y66mtbgN4");
   }
   public static function DB_SCHEME(){
-    return Config::get_env("DB_SCHEME", "sql7616663");
+    return Config::get_env("DB_SCHEME", "defaultdb");
   }
   public static function DB_PORT(){
-    return Config::get_env("DB_PORT", "3306");
+    return Config::get_env("DB_PORT", "25060");
   }
   public static function JWT_SECRET(){
     return Config::get_env("JWT_SECRET", "ezcb9s8UcF");
